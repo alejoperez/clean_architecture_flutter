5cee8ad5537cd719ab9cf9ffb9111a061f640844
Alejandro Perez <aperez@yellowpepper.com>
2020-06-02 17:18:57 -0500

    Test Unit Test Checker GitHub Action Flutter

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -2,7 +2,6 @@ import 'package:clean/flavor_config.dart';
 import 'package:clean/presentation/base/app/app.dart';
 import 'package:clean/presentation/base/app/dependency_injection.dart';
 import 'package:flutter/material.dart';
-import 'package:mockito/mockito.dart';
 
 void main() {
   FlavorConfig.appFlavor = Flavor.DEV;
